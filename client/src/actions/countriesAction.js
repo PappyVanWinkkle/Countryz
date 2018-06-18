cb945a23468b3c582946a3e61a8e36e2e696bf5e
PappyVanWinkkle <paulwchabz@gmail.com>
2018-06-18 13:49:57 +0100

    updated the api call

--- client/src/actions/countriesAction.js
+++ client/src/actions/countriesAction.js
@@ -8,7 +8,7 @@ import axios from "axios";
 import { GET_ALL_COUNTRIES } from "./types";
 
 export default function getCountries() {
-  const baseURL = `http://countryapi.gear.host/v1/Country/getCountries`;
+  const baseURL = `https://restcountries.eu/rest/v2/all`;
   return function(dispatch) {
     axios
       .get(`${baseURL}`)
