3caf5a7fa5e4fd111eec496fca2abff02d22aa5a
chabz <chabz@chabzs-MacBook.local>
2018-07-20 21:09:36 +0100

    Final changes before deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,37 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Countries from "./Components/countries";
-import Africa from "./Components/AfricanNations";
-import Landing from "./Components/Landing";
-import Asia from "./Components/Asia";
-import store from "./store";
-import Europe from "./Components/Europe";
-import Ocean from "./Components/Oceania";
-import Americas from "./Components/Americas";
-import { Provider } from "react-redux";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Countries from './Components/countries';
+import Africa from './Components/AfricanNations';
+import Landing from './Components/Landing';
+import Asia from './Components/Asia';
+import store from './store';
+import Europe from './Components/Europe';
+import Error from './Components/Error';
+import Ocean from './Components/Oceania';
+import Americas from './Components/Americas';
+import { Provider } from 'react-redux';
 
 class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <Router>
-          <div>
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/countries" component={Countries} />
-            <Route exact path="/africa" component={Africa} />
-            <Route exact path="/europe" component={Europe} />
-            <Route exact path="/asia" component={Asia} />
-            <Route exact path="/oceania" component={Ocean} />
-            <Route exact path="/americas" component={Americas} />
-          </div>
-        </Router>
-      </Provider>
-    );
-  }
+	render() {
+		return (
+			<Provider store={store}>
+				<Router>
+					<div>
+						<Switch>
+							<Route exact path="/" component={Landing} />
+							<Route exact path="/countries" component={Countries} />
+							<Route exact path="/africa" component={Africa} />
+							<Route exact path="/europe" component={Europe} />
+							<Route exact path="/asia" component={Asia} />
+							<Route exact path="/oceania" component={Ocean} />
+							<Route exact path="/americas" component={Americas} />
+							<Route component={Error} />
+						</Switch>
+					</div>
+				</Router>
+			</Provider>
+		);
+	}
 }
 
 export default App;
---
+++ client/src/Components/Error.js
@@ -0,0 +1,38 @@
+/*
+ ====================
+   404 PAGE FOR THE APP
+ *=================== */
+
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+const Errors = () => {
+	return (
+		<div>
+			<div className="container-fluid" style={{ background: '#F6F6F6' }}>
+				<div className="row">
+					<div className="col-sm-12">
+						<Link to="/" style={{ textDecoration: 'none' }}>
+							<button type="button" className="btn btn-outline-danger btn-lg mt-4">
+								Back Home
+							</button>
+						</Link>
+						<div>
+							<div className="container">
+								<h1
+									className="display-1 text-center text-danger"
+									style={{ marginTop: '10rem', fontWeight: '800' }}
+								>
+									404 Page Not Found
+								</h1>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Errors;
